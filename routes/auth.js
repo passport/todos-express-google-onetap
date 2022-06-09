5ac74f88f54cf7473bd9997a34c27bd0f2655551
Jared Hanson <jaredhanson@gmail.com>
2022-06-09 12:00:26 -0700

    Adjust to use Google One Tap.

--- routes/auth.js
+++ routes/auth.js
@@ -1,24 +1,14 @@
 var express = require('express');
 var passport = require('passport');
-var GoogleStrategy = require('passport-google-oidc');
+var GoogleOneTapStrategy = require('passport-google-one-tap').GoogleOneTapStrategy;
 var db = require('../db');
 
 
-// Configure the Google strategy for use by Passport.
-//
-// OAuth 2.0-based strategies require a `verify` function which receives the
-// credential (`accessToken`) for accessing the Facebook API on the user's
-// behalf, along with the user's profile.  The function must invoke `cb`
-// with a user object, which will be set at `req.user` in route handlers after
-// authentication.
-passport.use(new GoogleStrategy({
-  clientID: process.env['GOOGLE_CLIENT_ID'],
-  clientSecret: process.env['GOOGLE_CLIENT_SECRET'],
-  callbackURL: '/oauth2/redirect/google',
-  scope: [ 'profile' ]
-}, function verify(issuer, profile, cb) {
+passport.use(new GoogleOneTapStrategy({
+  clientID: process.env['GOOGLE_CLIENT_ID']
+}, function verify(profile, cb) {
   db.get('SELECT * FROM federated_credentials WHERE provider = ? AND subject = ?', [
-    issuer,
+    'https://accounts.google.com',
     profile.id
   ], function(err, row) {
     if (err) { return cb(err); }
@@ -30,7 +20,7 @@ passport.use(new GoogleStrategy({
         var id = this.lastID;
         db.run('INSERT INTO federated_credentials (user_id, provider, subject) VALUES (?, ?, ?)', [
           id,
-          issuer,
+          'https://accounts.google.com',
           profile.id
         ], function(err) {
           if (err) { return cb(err); }
@@ -87,31 +77,7 @@ router.get('/login', function(req, res, next) {
   res.render('login', { GOOGLE_CLIENT_ID: process.env['GOOGLE_CLIENT_ID'] });
 });
 
-router.post('/login/one-tap', function(req, res, next) {
-  console.log(req.body);
-});
-
-/* GET /login/federated/accounts.google.com
- *
- * This route redirects the user to Google, where they will authenticate.
- *
- * Signing in with Google is implemented using OAuth 2.0.  This route initiates
- * an OAuth 2.0 flow by redirecting the user to Google's identity server at
- * 'https://accounts.google.com'.  Once there, Google will authenticate the user
- * and obtain their consent to release identity information to this app.
- *
- * Once Google has completed their interaction with the user, the user will be
- * redirected back to the app at `GET /oauth2/redirect/accounts.google.com`.
- */
-router.get('/login/federated/google', passport.authenticate('google'));
-
-/*
-    This route completes the authentication sequence when Google redirects the
-    user back to the application.  When a new user signs in, a user account is
-    automatically created and their Google account is linked.  When an existing
-    user returns, they are signed in to their linked account.
-*/
-router.get('/oauth2/redirect/google', passport.authenticate('google', {
+router.post('/login/federated/google/one-tap', passport.authenticate('google-one-tap', {
   successReturnToOrRedirect: '/',
   failureRedirect: '/login'
 }));
